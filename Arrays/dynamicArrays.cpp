2a8172f5b97f8e2022bd2c9ce14c2d9d642c4aca
Kishen Sharma <kishen.r.sharma@gmail.com>
2021-06-22 18:26:55 -0700

    Update array, cleaned up cin

--- Arrays/dynamicArrays.cpp
+++ Arrays/dynamicArrays.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 void printArray(vector<int> const &a){
     cout<< "Array elements are: ";
-    for(int i=0; i < )
-https://www.tutorialspoint.com/how-to-print-out-the-contents-of-a-vector-in-cplusplus
+    for(int i=0; i < a.size(); i++)
+    cout << a.at(i) << " ";
+
 }
 
 int main(){
@@ -17,14 +18,30 @@ int main(){
     dynArray[2]= 1;
 
     cout << "The number of integers in the array is " << dynArray.size() <<endl;
-    cout << "Would you like to make modifications? Answer Yes or No" << endl;
+    cout << "Would you like to add an integer to the array? Answer Yes or No " << endl;
     
     //Variable to store user response
     string response;
     cin >> response;
 
-    if (response == 'Yes')
+    while (response != " ") {
+        if (response == "Yes"){
+            cout << "Input new value ";
+            int newValue;
+            cin >> newValue;
+            dynArray.push_back(newValue);
+            response= " ";
+        } else if (response =="No"){
+            response= " ";
+        } else{
+            cout << "Input correct response";
+            cout << "Would you like to add an integer to the array? Answer Yes or No" << endl;
+            cin >> response;
+        }
+    }
     
+    printArray(dynArray);
+
     return 0;
     
 
