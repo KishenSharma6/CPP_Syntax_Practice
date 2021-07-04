d6d988532cbf96249b35e7fa7bab228c40dc73da
Kishen Sharma <kishen.r.sharma@gmail.com>
2021-07-03 19:05:59 -0700

    Syntax

---
+++ ControlFlow/Loops.cpp
@@ -0,0 +1,21 @@
+// Create a simple calculator
+#include <iostream>
+
+using namespace std;
+
+void createArrays {
+    const int array1= 4;
+    const int array2= 5;
+
+}
+
+
+int main() {
+
+    createArrays()
+    int nums1[array1] = {2,3,5,-1}
+    int nums2[array2]= {3,4,24,13,4}
+
+    
+
+}
