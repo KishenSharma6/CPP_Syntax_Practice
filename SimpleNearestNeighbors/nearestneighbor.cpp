8f4298482022cf45c480f2bfaea26aa5447a4d7e
Kishen Sharma <kishen.r.sharma@gmail.com>
2021-07-13 17:57:11 -0700

    First commit

---
+++ SimpleNearestNeighbors/nearestneighbor.cpp
@@ -0,0 +1,8 @@
+#include <mlpack/core.hpp>
+#include <mlpack/methods/neighbor_search/neighbor_search.hpp>
+
+using namespace mlpack;
+using namespace mlpack::neighbor;
+using namespace mlpack::metric;
+
+
