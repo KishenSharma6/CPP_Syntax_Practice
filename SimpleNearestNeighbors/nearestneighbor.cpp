257a3849e2bedbfd5245189b92cd7d178e2c3e24
Kishen Sharma <kishen.r.sharma@gmail.com>
2021-07-14 09:29:42 -0700

    Init data and models

--- SimpleNearestNeighbors/nearestneighbor.cpp
+++ SimpleNearestNeighbors/nearestneighbor.cpp
@@ -1,8 +1,23 @@
 #include <mlpack/core.hpp>
 #include <mlpack/methods/neighbor_search/neighbor_search.hpp>
 
+using namespace std;
 using namespace mlpack;
 using namespace mlpack::neighbor;
 using namespace mlpack::metric;
 
 
+void mlModel () {
+    //Load data
+    arma::mat data;
+    data::Load("data.csv", data, true);
+
+    //Init NeighborSearch using ManhattanDistance
+    NeighborSearch<NearestNeighborSort, ManhattanDistance> nn(data);
+
+    //Init neightbors to store nearest neighbors
+
+    nn.Search
+    // https://www.mlpack.org/doc/stable/doxygen/sample.html
+
+}
