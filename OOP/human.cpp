7ad3a5695c3116039161d0303420cb25603b5ee1
Kishen Sharma <kishen.r.sharma@gmail.com>
2021-07-15 09:45:03 -0700

    Init Human Object

---
+++ OOP/human.cpp
@@ -0,0 +1,19 @@
+class Human 
+{
+    // Member attributes
+    string name;
+    string gender;
+    double age;
+    double weight;
+    double height_inches;   
+
+    // Member functions
+    void IntroduceSelf()
+};
+
+Human person1;
+person1.name = "Adam"
+person1.gender= 'Male'
+person1.age = 35
+person1.weight = 165
+person1.height_inches = 69
