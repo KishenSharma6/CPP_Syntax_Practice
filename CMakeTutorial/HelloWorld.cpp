16a600f23c86a3b2860b9c0b68a8eac6b0aa7b9f
Kishen Sharma <kishen.r.sharma@gmail.com>
2021-07-17 18:19:53 -0700

    push

--- CMakeTutorial/HelloWorld.cpp
+++ CMakeTutorial/HelloWorld.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-
+#include <mlpack/core.hpp>
+#include <mlpack/methods/neighbor_search/neighbor_search.hpp>
+using namespace mlpack;
+using namespace mlpack::neighbor; // NeighborSearch and NearestNeighborSort
+using namespace mlpack::metric;
 using namespace std;
 
 int main() {
