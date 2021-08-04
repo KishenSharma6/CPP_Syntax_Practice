1da66a3212563682318a848707ae28d9d2a255ad
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-08-03 18:24:49 -0700

    Updated attributes to human class

--- OOP/human.cpp
+++ OOP/human.cpp
@@ -1,19 +1,35 @@
-class Human 
-{
+#include <iostream>
+using namespace std;
+
+class Human {
+ public: //Be sure to set permissions to access this class   
     // Member attributes
-    string name;
-    string gender;
-    double age;
-    double weight;
-    double height_inches;   
+    string Name;
+    string Occupation;
+    string Company;
+    double Age;   
 
     // Member functions
-    void IntroduceSelf()
+    void IntroduceSelf(){
+        cout << "Hello, my name is " << Name << " and I am " << Age << " years old." << endl;
+        cout << "I am a " << Occupation << " at " << Company << endl;
+
+
+    };
 };
 
-Human person1;
-person1.name = "Adam"
-person1.gender= 'Male'
-person1.age = 35
-person1.weight = 165
-person1.height_inches = 69
+
+
+int main() {
+    Human person1;
+    person1.Name = "Adam";
+    person1.Occupation= "Manager";
+    person1.Company = "Google";
+    person1.Age= 35;
+
+
+
+
+    person1.IntroduceSelf();
+    return 0;
+}
