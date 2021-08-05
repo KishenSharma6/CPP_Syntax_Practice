fbb2c95a251d81a7e97b208d60641336a28db061
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-08-04 18:49:10 -0700

    refactored

--- OOP/human.cpp
+++ OOP/human.cpp
@@ -14,8 +14,12 @@ class Human {
     void IntroduceSelf(){
         cout << "Hello, my name is " << Name << "." << endl;
         cout << "I am a " << Occupation << " at " << Company << endl;
-        cout << "The programming languages I use are:" << endl;}
-        
+        cout << "The programming languages I use are:" << endl;
+        for (string language: ProgrammingLanguages){
+            cout << language << endl;
+            }
+    }
+    
     Human(string name, string occupation, string company, list <string> languages){ //build constructor
         Name= name;
         Occupation= occupation;
@@ -28,14 +32,9 @@ class Human {
 int main() {
     Human person1("Adam", "Manager", "Google", {"Python", "SQL", "C++"});
     Human person2("Michelle", "SWE", "Apple", {"Java", "C++",  "Swift"});
-
+    person2.ProgrammingLanguages.push_back("C#"); // missed one :)
     person1.IntroduceSelf();
-    for (string language: person1.ProgrammingLanguages){
-            cout << language << endl;
-        }
     person2.IntroduceSelf();
-    for (string language: person2.ProgrammingLanguages){
-            cout << language << endl;
-        }
+    
     return 0;
 }
