19e29419b94204129c553cbdb06deabab939933e
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-08-04 17:55:54 -0700

    Added Human constructor

--- OOP/human.cpp
+++ OOP/human.cpp
@@ -13,25 +13,28 @@ class Human {
     void IntroduceSelf(){
         cout << "Hello, my name is " << Name << " and I am " << Age << " years old." << endl;
         cout << "I am a " << Occupation << " at " << Company << endl;
-
-
     };
+    Human(string name, string occupation, string company, double age){
+        Name= name;
+        Occupation= occupation;
+        Company= company;
+        Age= age;
+
+    }
 };
 
-
-
 int main() {
-    Human person1;
-    person1.Name = "Adam";
-    person1.Occupation= "Manager";
-    person1.Company = "Google";
-    person1.Age= 35;
+    Human person1("Adam", "Manager", "Google", 35);
+    // person1.Name = "Adam";
+    // person1.Occupation= "Manager";
+    // person1.Company = "Google";
+    // person1.Age= 35;
 
-    Human person2;
-    person2.Name = "Michelle";
-    person2.Occupation= "Software Engineer";
-    person2.Company = "Apple";
-    person2.Age= 28;
+    Human person2("Michelle", "SWE", "Apple", 28);
+    // person2.Name = "Michelle";
+    // person2.Occupation= "Software Engineer";
+    // person2.Company = "Apple";
+    // person2.Age= 28;
 
     person1.IntroduceSelf();
     person2.IntroduceSelf();
