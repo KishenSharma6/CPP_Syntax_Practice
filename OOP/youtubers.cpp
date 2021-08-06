39224526d4811e3c37f3cd67fb0f396f430bd4bd
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-08-05 18:14:40 -0700

    Added encapsulation

--- OOP/youtubers.cpp
+++ OOP/youtubers.cpp
@@ -3,25 +3,46 @@
 using namespace std;
 
 class Youtube_Channel{
-public:
+private:
     string Channel_Name;
     string Owner;
     int Subscriber_Count;
+    list<string> Public_Videos;
 
-    void about_channel(){
-        cout <<"Hi, my name is " << Owner << " and I am the owner of " << Channel_Name << "." << endl;
-        cout << "I currently have " << Subscriber_Count << " subscribers on my channel.";
-    }
-
-    Youtube_Channel(string channel, string owner, int subs){
+public:
+    Youtube_Channel(string channel, string owner, int subs, list<string> videos = {}){
         Channel_Name = channel;
         Owner = owner;
         Subscriber_Count = subs;
+        Public_Videos= videos;
+    }
+    
+    void about_channel(){
+        cout <<"\nHi, my name is " << Owner << " and I am the owner of " << Channel_Name << "." << endl;
+        cout << "I currently have " << Subscriber_Count << " subscribers on my channel." << endl;
+        cout << "My most popular videos are:" <<endl;
+        for (string video:Public_Videos){
+            cout << video << endl;
+        }
+    }
+    
+    void add_subscriber(int num_subscribers){
+        Subscriber_Count += num_subscribers;
+
     }
 
-}
+    void add_videos(list<string> vids){
+        Public_Videos.merge(vids);
+    }
+
+};
 
 int main(){
-    
+    Youtube_Channel channel1("Peter McKinnon", "Peter", 50, {"The Mountains Will Remember Me"});
+    Youtube_Channel channel2("Joe Rogan Experience", "Joe", 40, {"Fight Companion", "Ep. 666: Duncan Trussel"});
+    channel1.add_videos({"Angels and Airways Documentary"});
+    channel1.add_subscriber(100);
+    channel1.about_channel();
+    channel2.about_channel();       
     return 0;
 }
