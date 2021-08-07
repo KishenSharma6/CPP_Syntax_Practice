6b90d75aec0710ff00faad81cdacfd60e227ced9
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-08-07 16:52:24 -0700

    Resolved constructor bug

--- OOP/youtubers.cpp
+++ OOP/youtubers.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 class Youtube_Channel{
-private:
+public:
     string Channel_Name;
     string Owner;
     int Subscriber_Count;
     list<string> Public_Videos;
-
 public:
     Youtube_Channel(string channel, string owner, int subs, list<string> videos = {}){
         Channel_Name = channel;
@@ -16,7 +15,6 @@ public:
         Subscriber_Count = subs;
         Public_Videos= videos;
     }
-    
     void about_channel(){
         cout <<"\nHi, my name is " << Owner << " and I am the owner of " << Channel_Name << "." << endl;
         cout << "I currently have " << Subscriber_Count << " subscribers on my channel." << endl;
@@ -25,14 +23,20 @@ public:
             cout << video << endl;
         }
     }
-    
     void add_subscriber(int num_subscribers){
         Subscriber_Count += num_subscribers;
 
     }
-
     void add_videos(list<string> vids){
         Public_Videos.merge(vids);
+    }
+
+};
+
+class New_Channel:public Youtube_Channel{
+    public:
+    New_Channel(string channel, string owner,int subs, list<string> videos = {}):Youtube_Channel(channel, owner, subs, videos){
+       
     }
 
 };
@@ -43,6 +47,9 @@ int main(){
     channel1.add_videos({"Angels and Airways Documentary"});
     channel1.add_subscriber(100);
     channel1.about_channel();
-    channel2.about_channel();       
+    channel2.about_channel();  
+
+    New_Channel channel3("JomaTech", "Joma", 4000, {"Every Tech CEO Ever", "Why I left Data Science"});     
+    channel3.about_channel();
     return 0;
 }
