9574b8a356908174e47ca03afc2cb3f90d95cee8
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-08-14 15:08:22 -0700

    First

---
+++ ML/mlpack_tutorial.cpp
@@ -0,0 +1,17 @@
+#include <mlpack/methods/kmeans/kmeans.hpp>
+#include <boost/serialization/serialization.hpp>
+#include <armadillo>
+#include <bits/stdc++.h>
+
+using namespace std;
+int k = 2; //amount of clusters
+int dim = 2;//dimensionality of program
+int samples = 50; 
+int max_iter = 10;//maximum number of iterations
+
+
+int main() {
+    return 0;
+}
+
+
