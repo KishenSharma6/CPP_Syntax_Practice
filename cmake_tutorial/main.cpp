64ea67b6ef3ad486cabdc06ac03d46cf5cb67249
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-08-16 18:36:22 -0700

    first

---
+++ cmake_tutorial/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+float add(float a, float b);
+
+using namespace std;
+
+int main(){
+    cout << "Code compiled successfully" << endl;
+    cout<< add(3.30,5.10) << endl;
+    return 0;
+}
